6fed3f2ccf0b10a7bf3dff690650e1d3ccb2ffe2
Andrija Petrovic <andrija.hers@gmail.com>
2015-11-25 20:17:11 +0100

    Typo fix

--- clientside.js
+++ clientside.js
@@ -2,7 +2,7 @@ function createClientSide(execlib,ParentServicePack) {
   'use strict';
   execlib.execSuite.UserRepresentation = require('./userrepresentationcreator')(execlib);
   var UserServiceSinkObtainerTask = require('./tasks/userservicesinkobtainercreator')(execlib),
-    GetInTask = require('./tasks/getIn')(execlib, UserServiceSinkObtainerTask);
+    GetInTask = require('./tasks/getIn')(execlib, UserServiceSinkObtainerTask),
     LetMeInTask = require('./tasks/letMeIn')(execlib, UserServiceSinkObtainerTask);
   return {
     SinkMap: require('./sinkmapcreator')(execlib, ParentServicePack),
